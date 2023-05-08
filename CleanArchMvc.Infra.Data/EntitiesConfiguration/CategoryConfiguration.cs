89e52d2c9e42198175c0af80ead0692441af4200
Marcelo Gomes <marcelogomes.0104@gmail.com>
2023-05-08 20:30:17 -0300

    DbContext, Category, Product

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id).HasName("PK_Category");
+            
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+
+            //Verifica o HasData e inseri ao criar o banco se não existir
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletronicos"),
+                new Category(3, "Acessorios")
+                );
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id).HasName("PK_Products");
+
+            builder.Property(n => n.Name).HasMaxLength(100).IsRequired();
+            builder.Property(d => d.Description).HasMaxLength(200).IsRequired();
+
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.HasOne(c => c.Category).WithMany(p => p.Products).
+                HasConstraintName("FK_Category_Products").HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
